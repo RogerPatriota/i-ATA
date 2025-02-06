2ac509a81bd520c277fed94e38cf21c34a1a2858
roger <rogerpatriota@gmail.com>
2025-02-06 09:10:23 -0300

    feat: add font settings and header comptn

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,13 @@
+import { GalleryHorizontalEnd } from "lucide-react"
+
 export function Header() {
-    return <div className="bg-slate-900">Header</div>
+    return <div className="w-auto h-20 m-10 p-6 flex flex-row items-center gap-2" >
+        <GalleryHorizontalEnd size={38} color="#4A3AFF" strokeWidth={2.25} className="mt-1.5" />
+        <div className="flex">
+            <h1 className="font-bold italic text-5xl font-liter text-[#4A3AFF]">I</h1>
+            <h1 className="font-bold italic text-5xl font-liter">-ATA</h1>
+        </div>
+    </div>
 }
 
 export default Header
