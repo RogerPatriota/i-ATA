4d610364700b6b7b9ed780c4a097092d836a089b
roger <rogerpatriota@gmail.com>
2025-02-12 23:39:28 -0300

    style: update progress bar animation progress

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Check } from 'lucide-react';
+{/* <div className="absolute top-10 left-0 w-full h-2 bg-gray-300 rounded-full"></div>
+
+
+<div
+  className="absolute top-10 left-0 h-2 bg-[#4A3AFF] rounded-full transition-all duration-500 ease-in-out"
+  style={{
+    width: `${((currentStep - 1) / (steps.length - 1)) * 100}%`,
+  }}
+></div> */}
 
 const ProgressSteps = (props) => {
+
     return (
-        <div className="w-140 flex justify-between m-auto px-5 mt-5">
+        <div className="relative w-145 flex justify-between m-auto mt-5">
+          <div className={`absolute h-2 top-8 w-full bg-gray-300 rounded-full`}></div>
+          <div className={`
+            absolute h-2 top-8 max-[140px] bg-[#4A3AFF] rounded-full transition-all duration-500 ease-in-out`}
+            style={{
+              width: `${((props.currentStep - 1) / (props.steps.length - 1)) * 100}%`
+            }}></div>
+
+
           {props.steps.map((step, index) => (
-            <div key={step.number} className="relative z-10 flex flex-col items-center mt-4">
+            <div key={step.number} className="relative flex flex-col items-center mt-4">
               <div className={
-                `w-10 h-10 rounded-full  flex items-center justify-center transition duration-500 ease-in-out
+                `w-10 h-10 rounded-full flex items-center justify-center transition duration-500 ease-in-out
                 ${index < props.currentStep ? 'bg-[#4A3AFF] text-white font-bold' : 'bg-[#EFF0F6]'}
                 `}>
                 {index + 1 < props.currentStep ? (
@@ -17,16 +35,26 @@ const ProgressSteps = (props) => {
                   )}
               </div>
 
-              {index < (props.steps.length - 1) && (
-                <div className='absolute top-1/2 left-full w-32 h-1'>
-                  <div className={`h-full transition-colors duration-400 ${index < props.currentStep - 1 ? 'bg-[#4A3AFF]' : 'bg-gray-300'}`}>
-                    <div className={`h-full w-15 ${(index + 1) == props.currentStep ? 'bg-[#4A3AFF]' : 'w-32'}`}></div>
-                  </div>
+              {/* {index < (props.steps.length - 1) && (
+                <div className='relative top-1/2 left-full w-32 h-1'>
+                  <div className={`absolute h-full transition-colors duration-400 ${index < props.currentStep - 1 ? 'bg-[#4A3AFF]' : 'bg-gray-300'}`}></div>
+                  <div className={`absolute h-full w-15 ${(index + 1) == props.currentStep ? 'bg-[#4A3AFF]' : 'w-32'}`}></div>
                 </div>
-              )}
+              )} */}
               
             </div>
           ))}
+          {/* <div className='absolute top-1/2  w-100 h-2 bg-gray-300 mt-1 ml-5 rounded-full'></div>
+          <div className={`absolue top-1/2 h-2 bg-[#4A3AFF] rounded-full transition-all duration-500 ease-in-out`}></div> */}
+
+          {/* <div className='absolute top-1/2 left-full w-32 h-1'>
+            <div className={`h-full relative ${0 < props.currentStep - 1 ? '' : 'bg-gray-300'}`}>
+              <div className={`h-full rounded-md bg-[#4A3AFF]  transition-all duration-500 ease-in-out ${
+                (0 + 1) == props.currentStep ? ' w-15' : 'w-32'
+                }`}>
+                </div>
+            </div>
+          </div>          */}
         </div>
       );
 };
