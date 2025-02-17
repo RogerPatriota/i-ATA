2a689a3f5bbef1651b4166ee07a0f2f630c53918
roger <rogerpatriota@gmail.com>
2025-02-16 22:57:21 -0300

    core: display filename

--- frontend/src/components/FileDrop.jsx
+++ frontend/src/components/FileDrop.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react"
+import { FileDown } from "lucide-react"
 
 
 export function FileDrop () {
+    const [filename, setFilename] = useState()
+
     return (
         <div className="w-180 mt-7 flex flex-col items-center gap-4  font-liter">
             <h3 className="text-2xl text-[#494853] ">Meeting record</h3>
             <p className="text-[#6F6C90] text-sm text-center">Please fill the field below with the meeting record, the notes will be automatic extract</p>
+
             <form action="" method="post" 
             encType="multipart/form-data" className="flex items-center justify-center">
-                <label 
-                    htmlFor="meeting-file" 
-                    className="w-180 h-90 pl-10 border-dashed border-3 bg-gray-50 hover:bg-gray-100 border-[#4A3AFF] 
-                    flex justify-center items-center gap-3 rounded-xl cursor-pointer">
-                        <span className="font-semibold text-base font-sans">Drop the file</span>
-                        or
-                        <span className="bg-[#4A3AFF] px-9 py-2 w-30 rounded-full text-sm text-white font-bold">Upload</span>
-                        <input type="file" name="file" id="meeting-file" placeholder="Upload"
-                        className="hidden cursor-pointer w-62 h-10 file:mr-4 file:font-semibold file:text-white  file:border-0 file:px-4 file:py-2 file:bg-[#4A3AFF]  file:rounded-3xl"
-                        required/>
-                </label>
+
+                <div className="relative overflow-hidden h-90 pl-5 border-dashed border-3 bg-gray-50 hover:bg-gray-100 border-[#4A3AFF] 
+                    flex flex-col justify-center items-center gap-3 rounded-xl cursor-pointer">
+                    {filename && (
+                        <div className="absolute top-32 w-100 py-3 flex items-center gap-3 text-xl border-1 border-[#EFF0F6] rounded-md shadow-xl">
+                            <FileDown size={60} className="ml-4 pr-3 border-r-1 border-gray-500"/>
+                            <p className="truncate max-w-full ">{filename}</p>
+                        </div>
+                    )}   
+                    <label 
+                        htmlFor="meeting-file" 
+                        className={`w-180 h-90 flex justify-center items-center gap-3 cursor-pointer
+                        ${filename ? 'mt-[20%]' : ''}`}>
+                            <span className="font-semibold text-lg font-sans">Drop the file</span>
+                            or
+                            <span className="bg-[#4A3AFF] py-2 px-8 w-32 rounded-full text-base text-white font-bold text-center">Upload</span>
+                            <input type="file" name="file" id="meeting-file" placeholder="Upload"
+                            onChange={(envent) => {setFilename(envent.target.files[0].name)}}
+                            className="hidden"
+                            required/>
+                    </label>
+
+                </div>
+
             </form>
         </div>
     )
--- frontend/src/components/ProgressContent.jsx
+++ frontend/src/components/ProgressContent.jsx
@@ -7,7 +7,7 @@ export function ProgressContent(props) {
     return (
         <div className="m-4 border-1 border-[#EFF0F6] shadow-2xl rounded-2xl w-[100%] h-160 flex flex-col items-center">
             <ProgressBar steps={props.steps} currentStep={props.currentStep}/>
-            { props.steps[props.currentStep - 1].label}
+            { props.steps[props.currentStep - 1].component}
             
         </div>
     )
